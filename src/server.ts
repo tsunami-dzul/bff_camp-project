e2f9e54e27cbc4f5d86e11a0a232345c6e95b7ea
Esdras Dzul <xdzul18@gmail.com>
2024-09-27 12:55:34 -0600

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -16,10 +16,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use('/api/auth', require('./routers/auth.router'));
-app.use('/api/carts', require('./routers/cart.router'));
-app.use('/api/products', require('./routers/products.router'));
-app.use('/api/categories', require('./routers/categories.router'));
+app.use('/auth', require('./routers/auth.router'));
+app.use('/carts', require('./routers/cart.router'));
+app.use('/products', require('./routers/products.router'));
+app.use('/categories', require('./routers/categories.router'));
 
 app.listen(process.env.PORT, () => {
   console.log(`Server is running on http://localhost:${process.env.PORT}`);
